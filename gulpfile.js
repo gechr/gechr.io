600d5692108811b9d511a835e66f4d82547c0fef
George Christou <gechrr@gmail.com>
2015-10-11 14:10:02 +0100

    gulpfile.js: Add 'sync' task

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,21 @@
 'use strict';
 
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    rsync = require('gulp-rsync');
 
 gulp.task('default', function() {
   // Where the magic happens...
+});
+
+gulp.task('sync', function() {
+  gulp.src('public/**')
+    .pipe(rsync({
+      root: 'public',
+      hostname: 'gechr.io',
+      destination: '/usr/share/nginx/html',
+      clean: true,
+      compress: true,
+      incremental: true,
+      recursive: true
+    }));
 });
